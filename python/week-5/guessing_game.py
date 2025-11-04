9385841659bd6cc3ee445d3893c6b8d19418995d
turganaliev <izatturganaliev@gmail.com>
2025-11-04 18:11:20 -0500

    updated W5

--- python/week-5/guessing_game.py
+++ python/week-5/guessing_game.py
@@ -1,18 +1,26 @@
 from random import randrange
 
+def guessing_game():
+    num = randrange(100)
 
-num = randrange(100)
+    for i in range(10):
+        print("Please guess your number from 1 to 100: ")
+        try:
+            guess = int(input())
+        except ValueError:
+            print("Please enter a number!")
+            continue
 
-for i in range(10):
-    print("Please guess your number from 1 to 100: ")
-    guess = int(input())
+        if guess == num:
+            print("Correct!")
+            break
+        elif guess > num:
+            print("too high")
+        else:
+            print("too low")
 
-    if guess == num:
-        print("Correct!")
-        break
-    elif guess > num:
-        print("too high")
     else:
-        print("too low")
+        print("Sorry, you run out of guesses!")
 
-print("Sorry, you run out of guesses!")
+if __name__ == '__main__':
+    guessing_game()
