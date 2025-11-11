1df496b93c411dfe860b8b8a61e7ec9cfb99087c
turganaliev <izatturganaliev@gmail.com>
2025-11-10 20:30:41 -0500

    done W7

--- python/week-7/wordcount.py
+++ python/week-7/wordcount.py
@@ -1,23 +1,25 @@
 import re
-
-
 from pathlib import Path
+import sys
 
-p = Path(__file__).with_name('alice.txt')
-with p.open('r') as f:
-    alice = f.read()
 
-p = Path(__file__).with_name('small.txt')
-with p.open('r') as f:
-    small = f.read()
+def read_file(filename):
+    p = Path(__file__).with_name(filename)
+    with p.open('r') as f:
+        content = f.read()
+        clean_content = content.replace('\n', ' ')
+        return clean_content
+
+def get_clean_text(text):
+        clean_text = re.sub(r'[^a-zA-Z\s]', ' ', text).lower()
+        words_list = clean_text.split(" ")
+        return words_list
 
 def print_words(text):
-    clean_text = re.sub(r'[^a-zA-Z\s]', ' ', text).lower()
-    words_list = clean_text.split(" ")
-
     res = {}
+    txt = get_clean_text(text)
 
-    for i in words_list:
+    for i in txt:
         if i in res and i != '':
             res[i] += 1
         elif i != '':
@@ -27,5 +29,42 @@ def print_words(text):
         print(f'{key}: {value}')
 
 
-# print_words(alice)
-print_words(small)
+def print_top(text):
+    res = {}
+    txt = get_clean_text(text)
+
+    for i in txt:
+        if i in res and i != '':
+            res[i] += 1
+        elif i != '':
+            res[i] = 1
+
+    sorted_res = sorted(res.items(), key=lambda item: item[1], reverse=True)
+
+    res_length = len(sorted_res)
+    if res_length < 20:
+        for i in range(res_length):
+           print(sorted_res[i])
+    else:
+        for i in range(20):
+           print(sorted_res[i])
+
+
+def main():
+  if len(sys.argv) != 3:
+    print('usage: ./wordcount.py {--count | --topcount} file')
+    sys.exit(1)
+
+  option = sys.argv[1]
+  filename = sys.argv[2]
+  if option == '--count':
+    print_words(read_file(filename))
+  elif option == '--topcount':
+    print_top(read_file(filename))
+  else:
+    print('unknown option: ' + option)
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+  main()
