81a8a71de40639f30f933656a1e4d1273356b65a
turganaliev <izatturganaliev@gmail.com>
2025-11-19 19:38:03 -0500

    solved fibonacci

---
+++ javaProgramming/fibonacci/Main.java
@@ -0,0 +1,10 @@
+package javaProgramming.fibonacci;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int n = 12;
+        System.out.println(solution.fib(n));
+    }
+}
