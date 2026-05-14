e00566839eefd58c674d77923670cb4697fd847e
turganaliev <izatturganaliev@gmail.com>
2026-05-14 15:37:36 +0600

    done 1

---
+++ data-structures-and-algorithms/week-16/README.md
@@ -0,0 +1,17 @@
+# Week 16 – DS & A Homework Report
+
+Status colors list:
+
+- 🟢 (Done)
+- 🟡 (In Progress)
+- 🔴 (Not Started)
+
+## Minimal Homework
+
+Problems (in lexicographical order) – status:
+
+- 🔴 [LC] Group Anagrams
+- 🔴 [LC] Longest Consecutive Sequence
+- 🔴 [LC] Subarray Sum Equals K
+- 🔴 [LC] Top K Frequent Elements
+- 🟢 [LC] Two Sum
---
+++ data-structures-and-algorithms/week-16/TwoSum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = target - nums[i];
+            if (map.containsKey(n)) return new int[]{map.get(n), i};
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
