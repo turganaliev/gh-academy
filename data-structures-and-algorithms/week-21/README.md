7092392c2d34775212584c7a2a0a2976bb10afbb
turganaliev <izatturganaliev@gmail.com>
2026-07-01 06:04:29 +0600

    done 206

---
+++ data-structures-and-algorithms/week-21/README.md
@@ -0,0 +1,18 @@
+# Week 21 – DS & A Homework Report
+
+Status colors list:
+
+- 🟢 (Done)
+- 🟡 (In Progress)
+- 🔴 (Not Started)
+
+## Minimal Homework
+
+Problems (in lexicographical order) – status:
+
+- 🔴 [LC] Add Two Numbers
+- 🔴 [LC] Design Browser History
+- 🟡 [LC] Merge Two Sorted Lists
+- 🔴 [LC] Remove Nth Node From End of List
+- 🟢 [LC] Reverse Linked List
+- 🔴 [LC] Reverse Linked List II
---
+++ data-structures-and-algorithms/week-21/ReverseLinkedList.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return recurse(head, null);
+    }
+    private ListNode recurse(ListNode curr, ListNode prev) {
+        if (curr == null) return prev;
+        ListNode next = curr.next;
+        curr.next = prev;
+        return recurse(next, curr);
+    }
+}
